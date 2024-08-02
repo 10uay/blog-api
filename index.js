0470e1cffebcc0c0fee502e35a0f6b2fed86469e
Louay Deeb <104592607+10uay@users.noreply.github.com>
2024-08-02 16:47:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -29,8 +29,15 @@ const allowedOrigins = process.env.NODE_ENV === 'prodution' ? prodOrigins : 'htt
 //     credentials: true,
 //     methods: ['GET', 'POST', 'PUT', 'DELETE']
 // }))
-app.use(cors());
-
+app.use(
+  cors({
+    origin: "https://blog-louay-05.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use(cookieParser());
